d5e3f3cf4cc5dd3a57846ea25bf7a67825ef2fcf
server <wirelesstaco@gmail.com>
2016-05-14 01:13:40 -0500

    Initial set up

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
-var img;
+var mic;
+var amp;
 
-function preload(){
-	img = loadImage("media/lunar.jpg");
+var scale =1.0;
+
+function setup(){
+	createCanvas(440,440);
+	background(0);
+
+	mic = new p5.AudioIn();
+	mic.start();
+
+	amp = new p5.Amplitude();
+	amp.setInput(mic);
 }
-function setup() {
-	createCanvas(500,500);
+
+function draw(){
+	print(amp.getLevel());
+	noStroke();
+	fill(0,10);
+	rect(0,0,width,height);
+	scale = map(amp.getLevel(), 0, 1.0,10,width);
+		//draw circle
+	fill(255);
+	ellipse(width/2,height/2,scale,scale);
 }
-function draw() {
-	image(img,0,0);
-}
+
+

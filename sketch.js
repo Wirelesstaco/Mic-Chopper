ee70c87ac5bd3cc1c98736c564e797705e735e70
wirelesstaco <wirelesstaco@gmail.com>
2016-05-18 17:35:59 -0500

    Added Mic sensitivy and friction

--- sketch.js
+++ sketch.js
@@ -1,28 +1,77 @@
 var mic;
 var amp;
 
-var scale =1.0;
+var micThreashold =.01;
+
+var chopper;
+
+var boost = .156;
+var gravity = 0.17;
+
+
 
 function setup(){
 	createCanvas(440,440);
 	background(0);
-
+  
 	mic = new p5.AudioIn();
 	mic.start();
 
 	amp = new p5.Amplitude();
 	amp.setInput(mic);
+
+	chopper = new Chopper();
+	
 }
 
 function draw(){
-	print(amp.getLevel());
-	noStroke();
-	fill(0,10);
-	rect(0,0,width,height);
-	scale = map(amp.getLevel(), 0, 1.0,10,width);
-		//draw circle
-	fill(255);
-	ellipse(width/2,height/2,scale,scale);
+  background(51);
+  noStroke();
+  chopper.fly();
+  chopper.display();
+  chopper.checkEdges();
+  
 }
 
+function Chopper(){
+  //Chopper Setup
+  this.pos = createVector(200,height /2);
+  this.vel = createVector(0,0);
+  
+  
+  // Draw Copter
+  this.display = function () {
+    if(amp.getLevel() > micThreashold){
+    this.acc = createVector(0,-boost);
+	    fill(0,255,0);
+    }else{
+      fill(255,0,0);
+      this.acc = createVector(0,gravity);
+    }
+    
+	  ellipse(this.pos.x,this.pos.y,30,30);
+
+  }
+  this.fly = function (){
+    
+    
+    this.vel.limit(6);
+    
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel)
+    
+    this.vel.mult(0.98);
+    
+    }
+  this.checkEdges = function () {
+    if (this.pos.y > height){
+      this.vel.y = 0;
+      this.pos.y = height;
+     } else if (this.pos.y < 0){
+       this.vel.y = 0;
+       this.pos.y = 0;
+     } 
+  }
+}
 
